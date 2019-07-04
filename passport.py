c0d9496ec86b44164ea3a19dbc75c3e0aa235809
Lebedev <38814315+volsn@users.noreply.github.com>
2019-07-04 09:18:57 +0300

    Position based text areas classification

--- passport.py
+++ passport.py
@@ -1,12 +1,10 @@
 import cv2
-import imutils
-from imutils import rotate_bound
-from imutils.object_detection import non_max_suppression
-from imutils.contours import sort_contours
 import os
 import re
-import math
-import random
+import sys
+import imutils
+from imutils.object_detection import non_max_suppression
+from imutils.contours import sort_contours
 import numpy as np
 from pytesseract import image_to_string
 
@@ -165,70 +163,141 @@ def cut_passport(image):
     """
     return masked
 
-def read_name(mask, bottom):
 
-    (H,W) = bottom.shape[:2]
+def authority_text_boxes(image, boxes, rW, rH):
+
+    mask = np.zeros(image.shape[:2],dtype=np.uint8)
+    mask_text_zones = np.zeros(image.shape[:2],dtype=np.uint8)
+    (H,W) = image.shape[:2]
+
+    # loop over the bounding boxes
+    for (startX, startY, endX, endY) in boxes:
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+
+        if startX > 0.2 * W and endX < 0.8 * W:
+
+            mask[startY:endY,:] = 255
+            mask_text_zones[startY:endY,startX:endX] = 255
 
     img_cnt, cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-    orig = bottom.copy()
 
-    text = []
-    masked = np.zeros(orig.shape[:2],dtype=np.uint8)
-    if len(cnts) >= 3:
+    temp_masks = []
+    authority = []
 
-        (cnts, boundingBoxes) = sort_contours(cnts, method='top-to-bottom')
+    (cnts, boundingBoxes) = sort_contours(cnts, method='top-to-bottom')
+    for k, (cnt, box)in enumerate(zip(cnts, boundingBoxes)):
 
-        ROIs = []
-        for j, (cnt, box)in enumerate(zip(cnts, boundingBoxes)):
+        temp_mask = np.zeros(mask.shape[:2],dtype=np.uint8)
+        temp_mask = cv2.drawContours(temp_mask, [cnt], -1, (255,255,255), -1)
 
-            temp_mask = np.zeros(mask.shape[:2],dtype=np.uint8)
-            temp_mask = cv2.drawContours(temp_mask, [cnt], -1, (255,255,255), -1)
+        temp_masks.append(temp_mask)
 
-            (x, y, w, h) = box
+        line_test = cv2.bitwise_and(temp_mask, mask_text_zones)
 
-            if x >= 0.1 * W:
+        (H,W) = image.shape[:2]
 
-                """x -= int(0.1 * w)
-                w += int(0.1 * w) * 2"""
-                x -= 100
-                w += 200
-                y -= int(0.5 * h)
-                h += int(0.5 * h) * 2
+        left_border = np.where(line_test == 255)[1].min()
+        right_border = np.where(line_test == 255)[1].max()
+        top_border = np.where(line_test == 255)[0].min()
+        bottom_border = np.where(line_test == 255)[0].max()
 
-                temp_mask[y:y+h,x:x+w] = 255
+        """left_border = max(left_border - 250, int(0.2 * W))
+        right_border = min(right_border + 250, int(0.8 * W))"""
 
-                #cv2.rectangle(orig, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        left_border = 0
+        right_border = W
+        top_border = max(top_border - 20, 0)
+        bottom_border = min(bottom_border + 20, H)
 
-                roi = get_segment_crop(orig, orig, mask=temp_mask)
-                ROIs.append(roi)
+        line_mask = np.zeros(image.shape[:2], dtype=np.uint8)
+        line_mask[top_border:bottom_border, left_border:right_border] = 255
 
-                """cv2.imwrite('Mask/{}.png'.format(name), orig)
-                cv2.imwrite('ROIs/{}_{}.png'.format(name,j),roi)
-                """
+        masked_line = get_segment_crop(image, image, mask=line_mask)
+        authority.append(masked_line)
 
-                roi_text = read_text_from_blob(roi)
+    # ROIs, mask, mask_text_zones
+    return authority
 
-                text.append(roi_text + '\n')
 
-    #print(text)
-    result = []
-    for word in text[:3]:
-        word = word.replace('\n', ' ')
-        word = re.sub(r'[^а-яА-Я ]+', '', word)
-        potentials = word.split()
-        if potentials != []:
-            result.append(sorted(potentials, key=len)[-1])
+def name_text_boxes(image, boxes, rW, rH):
 
-    return result
+    mask = np.zeros(image.shape[:2],dtype=np.uint8)
+    mask_text_zones = np.zeros(image.shape[:2],dtype=np.uint8)
+    (H,W) = image.shape[:2]
+
+    # loop over the bounding boxes
+    for (startX, startY, endX, endY) in boxes:
+        # scale the bounding box coordinates based on the respective
+        # ratios
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+
+        if startX >= 0.1 * W and endX <= 0.8 * W:
+
+            mask[startY:endY,:] = 255
+            mask_text_zones[startY:endY,startX:endX] = 255
+
+    img_cnt, cnts, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+    temp_masks = []
+    text_boxes = []
+
+    (cnts, boundingBoxes) = sort_contours(cnts, method='top-to-bottom')
+    for k, (cnt, box)in enumerate(zip(cnts, boundingBoxes)):
+
+        temp_mask = np.zeros(mask.shape[:2],dtype=np.uint8)
+        temp_mask = cv2.drawContours(temp_mask, [cnt], -1, (255,255,255), -1)
+
+        temp_masks.append(temp_mask)
+
+        line_test = cv2.bitwise_and(temp_mask, mask_text_zones)
+
+        (H,W) = image.shape[:2]
+
+        if k < 3:
+
+            left_border = np.where(line_test == 255)[1].min()
+            right_border = np.where(line_test == 255)[1].max()
+            top_border = np.where(line_test == 255)[0].min()
+            bottom_border = np.where(line_test == 255)[0].max()
+
+            left_border = max(left_border - 150, 0)
+            right_border = min(right_border + 150, W)
+            top_border = max(top_border - 15, 0)
+            bottom_border = min(bottom_border + 15, H)
+
+        else:
+
+            top_border = np.where(line_test == 255)[0].min()
+            bottom_border = np.where(line_test == 255)[0].max()
+
+            left_border = max(left_border - 150, 0)
+            right_border = min(right_border + 150, W)
+            top_border = max(top_border - 15, 0)
+            bottom_border = min(bottom_border + 15, H)
+
+
+        line_mask = np.zeros(temp_mask.shape[:2], dtype=np.uint8)
+        line_mask[top_border:bottom_border, left_border:right_border] = 255
+
+        masked_line = get_segment_crop(image, image, mask=line_mask)
+        text_boxes.append(masked_line)
+
+    # ROIs, mask, mask_text_zones
+    return text_boxes
+
 
 def locate_text(image):
 
     orig = image.copy()
     (H, W) = image.shape[:2]
-
-    gray = cv2.cvtColor(orig.copy(), cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    raw_text = image_to_string(thresh, lang='rus')
 
     (newW, newH) = (320, 320)
     rW = W / float(newW)
@@ -296,91 +365,147 @@ def locate_text(image):
             rects.append((startX, startY, endX, endY))
             confidences.append(scoresData[x])
 
-    mask = np.zeros(orig.shape[:2],dtype=np.uint8)
-    ROIs = []
     boxes = non_max_suppression(np.array(rects), probs=confidences)
-
     boxes = sorted(boxes,key=lambda x:x[1])
 
-    # loop over the bounding boxes
-    for (startX, startY, endX, endY) in boxes:
-        # scale the bounding box coordinates based on the respective
-        # ratios
-        startX = int(startX * rW)
-        startY = int(startY * rH)
-        endX = int(endX * rW)
-        endY = int(endY * rH)
+    return boxes, (rW, rH)
 
-        w = endX - startX
-        h = endY - startY
 
-        #if w > 50 and h > 15:
-
-        endX += 25
-        startX -= 25
-        """startY -= 5
-        endY += 5"""
-
-        mask[startY:endY,startX:endX] = 255
-        ROIs.append(orig[startY:endY,startX:endX].copy())
-
-    located = cv2.bitwise_and(orig, orig, mask=mask)
-
-    return mask, located
-
-def read_text_from_blob(image):
-
-    roi = image.copy()
+def read_text(roi, type_):
 
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.medianBlur(gray,3)
-    ret, thresh = cv2.threshold(blurred,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    roi_text = image_to_string(thresh, lang='rus')
+    blurred = cv2.medianBlur(gray, 3)
+    eroded = cv2.erode(blurred, (3,3), iterations=1)
+    dilated = cv2.dilate(eroded, (3,3), iterations=1)
+    ret, thresh = cv2.threshold(dilated,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    text = image_to_string(thresh, lang='rus').replace('\n', ' ')
 
-    if roi_text == '':
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray,(5,5),0)
-        ret, thresh = cv2.threshold(blurred,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        roi_text = image_to_string(thresh, lang='rus')
+    if type_ == 'auth':
 
-    if roi_text == '':
-        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(gray,(1,1),0)
-        dilated = cv2.dilate(blurred, (3,3), iterations=1)
-        ret, thresh = cv2.threshold(dilated,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        roi_text = image_to_string(thresh, lang='rus')
+        text = re.sub(r'[^А-Я\.\d -]+', '', text)
 
-    return roi_text
+        ALLOWED_SHORT_WORDS = ['и', 'в', 'по']
+        authority = ''
+        for word in text.split():
+            if len(word) > 2 or word.lower() in ALLOWED_SHORT_WORDS:
+                authority += word + ' '
 
-def analyze_passport(image):
+        text = authority
 
-    orig = image.copy()
-    orig = cut_passport(orig)
-    orig = rotate_passport(orig)
-    passport = passport_border(orig)
+    elif type_ == 'birth':
 
-    (H,W) = passport.shape[:2]
+        text = re.sub(r'[^А-Я\. -]+', '', text)
 
-    bottom = passport[H//2:H,W//3:W]
-    mask,located = locate_text(bottom)
+        ALLOWED_SHORT_WORDS = ['с.']
+        birth_place = ''
+        for word in text.split():
+            if len(word) > 2 or word.lower() in ALLOWED_SHORT_WORDS:
+                birth_place += word + ' '
 
-    pasport = {'name': '', 'surname': '', 'patronymic_name': '', 'authority': ''}
+        text = birth_place
 
-    full_name = read_name(mask, bottom)
-    """print(full_name)
-    if len(full_name) == 3:
-        passport['surname'] = full_name[0]
-        passport['name'] = full_name[1]
-        passport['patronymic_name'] = full_name[2]
-    else:
-        passport['FIO'] = full_name"""
+    elif type_ == 'number':
+        text = re.sub(r'[^\d. -]+', '', text)
 
-    return full_name
+    elif type_ == 'name':
 
-    """
-    # TODO Read Authority
-    """
+        for word in text.split():
 
-    """
-    # TODO Parse Numerical Data
-    """
+            word = re.sub(r'[^а-яА-Я ]+', '', word)
+            potentials = word.split()
+            if potentials != []:
+                text = sorted(potentials, key=len)[-1]
+
+    return text
+
+
+def read_side(image):
+
+    image = image.copy()
+    image = imutils.rotate_bound(image, angle=-90)
+
+    (h,w) = image.shape[:2]
+
+    side = image[:h//10,:]
+
+    gray = cv2.cvtColor(side, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.medianBlur(gray, 3)
+    eroded = cv2.erode(blurred, (3,3), iterations=1)
+    dilated = cv2.dilate(eroded, (3,3), iterations=1)
+    ret, thresh = cv2.threshold(dilated,0,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    text = image_to_string(thresh, lang='rus').replace('\n', ' ')
+
+    output = {'series': '', 'number': ''}
+
+    series = re.search(r'\d{2} \d{2}', text)
+    if series is not None:
+        output['series'] = series[0]
+
+    number = re.search(r'\d{6}', text)
+    if number is not None:
+        output['number'] = number[0]
+
+    return output
+
+
+def analyze_passport(passport):
+
+    image = passport.copy()
+    (h,w) = image.shape[:2]
+
+    top = image[0:h//2,:]
+    boxes, (rW, rH) = locate_text(top)
+    top = authority_text_boxes(top, boxes, rW, rH)
+
+    bottom = image[h//2:h,w//3:w]
+    boxes, (rW, rH) = locate_text(bottom)
+    bottom = name_text_boxes(bottom, boxes, rW, rH)
+
+    image_cut = {'top': [], 'surname': np.ones((1,1), dtype=np.uint8), 'name': np.ones((1,1), dtype=np.uint8), \
+                 'patronymic': np.ones((1,1), dtype=np.uint8), \
+                 'birth_date': np.ones((1,1), dtype=np.uint8),'birth_place': []}
+
+    ocr_result = {'top': '', 'surname': '', 'name': '', 'patronymic': '', 'birth_date': '','birth_place': '', \
+                         'issue_date': '', 'issue_code': ''}
+
+    for line in top:
+        image_cut['top'].append(line)
+        ocr_result['top'] += read_text(line, type_='auth') + ' '
+
+    if ocr_result['top'] != '':
+
+        issue_date = re.search(r'\d{2}\.\d{2}\.\d{4}', ocr_result['top'])
+        if issue_date is not None:
+            ocr_result['issue_date'] = issue_date[0]
+
+        issue_code = re.search(r'\d{3}-\d{3}', ocr_result['top'])
+        if issue_code is not None:
+            ocr_result['issue_code'] = issue_code[0]
+
+
+    if len(bottom) > 3:
+
+        image_cut['surname'] = bottom[0]
+        image_cut['name'] = bottom[1]
+        image_cut['patronymic'] = bottom[2]
+
+        ocr_result['surname'] = read_text(bottom[0], type_='name').upper()
+        ocr_result['name'] = read_text(bottom[1], type_='name').upper()
+        ocr_result['patronymic'] = read_text(bottom[2], type_='name').upper()
+
+
+    if len(bottom) > 4:
+
+        image_cut['birth_date'] = bottom[3]
+        ocr_result['birth_date'] = read_text(bottom[3], type_='number')
+
+        for line in bottom[4:]:
+            image_cut['birth_place'].append(line)
+            #print(read_text(line, type_='birth') + ' ')
+            ocr_result['birth_place'] += read_text(line, type_='birth') + ' '
+
+    ocr_result.update(read_side(image))
+
+    result = {'ocr_result': ocr_result, 'cut': image_cut}
+
+    return result['ocr_result']
